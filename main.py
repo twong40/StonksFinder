af3973fc8068784a6f9b1706bce026ec0ac3b4f7
thomaswong1000@gmail.com <twong40@ucsc.edu>
2021-03-20 20:28:34 -0700

    added search and insert into mongo, need to add stricter checks and add more reddit post info

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from getAccessToken import getToken
 from databaseOperations import createClient, insertOne, insertMany
 from credentials import stockApiKey
+from datetime import datetime
 import requests
 import itertools
 import time
 
 def main():
+    db = createClient()
+    stockSymbols = searchReddit()
+    jsonData = searchStockInfo(stockSymbols)
+    print("Phase 3: Saving to MongoDb")
+    insertOne(db, jsonData)
+
+def searchReddit():
+    print("Phase 1: Scanning subreddits")
     seenTitles = set()
     stockSymbols = dict()
-    db = createClient()
     newToken = getToken()
     headers = {"Authorization": "bearer " + newToken, "User-Agent": "pythonScript:StonksFinder/0.1 by thiccc69696969"}
     payload = {'limit': 100}
     redditEndpoint = "https://oauth.reddit.com/r/<SUBREDDIT>/<FILTER>"
     stockSubreddits = ['stocks', 'investing', 'wallstreetbets']
     stockFilters = ['rising', 'new', 'top', 'best']
-    stockQuoteEndpoint = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=<SYMBOL>&apikey=" + stockApiKey
     # Iterate through the possible subreddits and filters
-    for subreddit, filter in itertools.product(stockSubreddits, stockFilters):
+    for subreddit, filterKey in itertools.product(stockSubreddits, stockFilters):
         # Iterate through at most 4 papges
         for i in range(4):
-            print("PAGE: " + str(i))
-            response = requests.get(redditEndpoint.replace("<SUBREDDIT>", subreddit).replace("<FILTER>", filter), headers=headers, params=payload)
+            response = requests.get(redditEndpoint.replace("<SUBREDDIT>", subreddit).replace("<FILTER>", filterKey), headers=headers, params=payload)
             for i in range(len(response.json()['data']['children'])):
                 title = response.json()['data']['children'][i]['data']['title']
                 # If this is a new title, search for the ticker and add it to the map with the amount seen
@@ -41,17 +47,30 @@ def main():
             payload['after'] = response.json()['data']['after']
             if payload['after'] is None:
                 break
+    return stockSymbols
+
+def searchStockInfo(stockSymbols):
+    print("Phase 2: Searching for stock info")
+    stockQuoteEndpoint = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=<SYMBOL>&apikey=" + stockApiKey
     counter = 0
+    jsonData = {"timeStamp" : datetime.now()}
+    totalData = []
     for ticker in stockSymbols:
         # Api only allows 5 calls per minute, 500 calls per day
         if counter % 5 == 0:
             counter = 0
             time.sleep(60)
         stockResponse = requests.get(stockQuoteEndpoint.replace("<SYMBOL>", ticker))
-        if stockResponse.json()["Global Quote"] == None:
-            stockSymbols.pop(ticker)
-        print(stockResponse.json())
+        if "Global Quote" in stockResponse.json() and len(stockResponse.json()["Global Quote"]) != 0:
+            stockData = {"symbol":ticker, "numberOfMentions":stockSymbols[ticker]}
+            extraStockData = dict()
+            # Clean up json response data
+            for key in stockResponse.json()["Global Quote"]:
+                extraStockData[key[4:]] = stockResponse.json()["Global Quote"][key]
+            totalData.append({**stockData, **extraStockData})
         counter+= 1
+    jsonData["data"] = totalData
+    return jsonData
 
 
 if __name__ == "__main__":
