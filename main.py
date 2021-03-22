bebc5dc30a5226ef5bbef976119bd0aff4686091
thomaswong1000@gmail.com <twong40@ucsc.edu>
2021-03-21 20:01:19 -0700

    added filter for symbols, more info in reddit posts

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from getAccessToken import getToken
 from databaseOperations import createClient, insertOne, insertMany
-from credentials import stockApiKey
+from credentials import stockApiKey, userAgent
 from datetime import datetime
 import requests
 import itertools
 import time
+import re
 
 def main():
     db = createClient()
@@ -13,12 +14,18 @@ def main():
     print("Phase 3: Saving to MongoDb")
     insertOne(db, jsonData)
 
+def isValidSymbol(symbolToCheck):
+    regex = re.compile('[0-9@_!#%^&*()<>?/\|}{~:]')
+    if len(symbolToCheck) > 0 and len(symbolToCheck) <= 5 and regex.search(symbolToCheck) == None:
+        return True
+    return False
+
 def searchReddit():
     print("Phase 1: Scanning subreddits")
     seenTitles = set()
     stockSymbols = dict()
     newToken = getToken()
-    headers = {"Authorization": "bearer " + newToken, "User-Agent": "pythonScript:StonksFinder/0.1 by thiccc69696969"}
+    headers = {"Authorization": "bearer " + newToken, "User-Agent": userAgent}
     payload = {'limit': 100}
     redditEndpoint = "https://oauth.reddit.com/r/<SUBREDDIT>/<FILTER>"
     stockSubreddits = ['stocks', 'investing', 'wallstreetbets']
@@ -26,23 +33,38 @@ def searchReddit():
     # Iterate through the possible subreddits and filters
     for subreddit, filterKey in itertools.product(stockSubreddits, stockFilters):
         # Iterate through at most 4 papges
-        for i in range(4):
+        for i in range(5):
             response = requests.get(redditEndpoint.replace("<SUBREDDIT>", subreddit).replace("<FILTER>", filterKey), headers=headers, params=payload)
             for i in range(len(response.json()['data']['children'])):
-                title = response.json()['data']['children'][i]['data']['title']
+                responseData = response.json()['data']['children'][i]['data']
+                title = responseData['title']
                 # If this is a new title, search for the ticker and add it to the map with the amount seen
                 if title not in seenTitles:
                     seenTitles.add(title)
                     splitTitle = title.split()
                     for word in splitTitle:
                         index = word.find("$")
-                        if index != -1:
-                            ticker = word[index + 1:]
+                        if index != -1 and isValidSymbol(word):
+                            ticker = word[index + 1:].upper()
                             value = stockSymbols.get(ticker)
                             if value is None:
-                                stockSymbols[ticker] = 1
+                                redditStockData = dict()
+                                redditStockData["symbol"] = ticker
+                                redditStockData["numberOfMentions"] = 1
+                                redditStockData["ups"] = [responseData["ups"]]
+                                redditStockData["upvote_ratio"] = [responseData["upvote_ratio"]]
+                                redditStockData["total_awards_received"] = [responseData["total_awards_received"]]
+                                redditStockData["gilded"] = [responseData["gilded"]]
+                                redditStockData["num_comments"] = [responseData["num_comments"]]
+                                stockSymbols[ticker] = redditStockData
                             else:
-                                stockSymbols[ticker] = value + 1
+                                value["numberOfMentions"] = value["numberOfMentions"] + 1
+                                value["ups"].append(responseData["ups"])
+                                value["upvote_ratio"].append(responseData["upvote_ratio"])
+                                value["total_awards_received"].append(responseData["total_awards_received"])
+                                value["gilded"].append(responseData["gilded"])
+                                value["num_comments"].append(responseData["num_comments"])
+                                stockSymbols[ticker] = value
             # No Page afterwards
             payload['after'] = response.json()['data']['after']
             if payload['after'] is None:
@@ -62,16 +84,14 @@ def searchStockInfo(stockSymbols):
             time.sleep(60)
         stockResponse = requests.get(stockQuoteEndpoint.replace("<SYMBOL>", ticker))
         if "Global Quote" in stockResponse.json() and len(stockResponse.json()["Global Quote"]) != 0:
-            stockData = {"symbol":ticker, "numberOfMentions":stockSymbols[ticker]}
             extraStockData = dict()
             # Clean up json response data
             for key in stockResponse.json()["Global Quote"]:
                 extraStockData[key[4:]] = stockResponse.json()["Global Quote"][key]
-            totalData.append({**stockData, **extraStockData})
+            totalData.append({**stockSymbols[ticker], **extraStockData})
         counter+= 1
     jsonData["data"] = totalData
     return jsonData
-
 
 if __name__ == "__main__":
     main()
